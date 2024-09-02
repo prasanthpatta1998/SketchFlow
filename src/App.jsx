068caf6e75fced77c19128eec58868a30043e64d
prasanth <prasanth@example.com>
2024-09-02 13:24:13 +0530

    Sketchflow application completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Tldraw } from "tldraw";
+import "tldraw/tldraw.css";
+
+const App = () => {
+  return (
+    <div>
+      <Tldraw className="tldraw"/>
+    </div>
+  );
+};
+
+export default App;
